3edd9b2d18905306b633e22ea9e504309fd250f4
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2024-09-29 13:10:34 +0300

    minor: fix compile

--- src/main/kotlin/com/example/personalaccount/service/PersonalAccountManager.kt
+++ src/main/kotlin/com/example/personalaccount/service/PersonalAccountManager.kt
@@ -1,7 +1,6 @@
 package com.example.personalaccount.service
 
 import com.example.personalaccount.database.AccountEntity
-import com.example.personalaccount.model.FriendshipStatus
 import java.util.*
 
 
