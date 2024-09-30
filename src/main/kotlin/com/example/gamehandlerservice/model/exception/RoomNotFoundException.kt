dadcd5b74b37638529ffb97183e3ec6936f95860
Ivan Sobolev <isobolev@solanteq.com>
2024-09-30 22:54:44 +0300

    test: delete unnecessary exceptions

--- src/main/kotlin/com/example/gamehandlerservice/model/exception/RoomNotFoundException.kt
+++
@@ -1,4 +0,0 @@
-package com.example.gamehandlerservice.model.exception
-
-class RoomNotFoundException : ModelException() {
-}
--- src/main/kotlin/com/example/gamehandlerservice/model/exception/TimeoutException.kt
+++
@@ -1,4 +0,0 @@
-package com.example.gamehandlerservice.model.exception
-
-class TimeoutException : ModelException() {
-}
