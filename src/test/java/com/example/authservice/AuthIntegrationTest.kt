0353cce471d9b21a624d4836bda7c709843fecc5
Ivan Sobolev <isobolev@solanteq.com>
2024-09-30 08:36:08 +0300

    test: fix auth e2e

--- src/test/java/com/example/authservice/AuthIntegrationTest.kt
+++ src/test/java/com/example/authservice/AuthIntegrationTest.kt
@@ -3,18 +3,19 @@ package com.example.authservice
 import com.example.authservice.dto.AuthenticationResponse
 import com.example.authservice.dto.CredentialsRequest
 import com.example.common.E2EDbInit
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.get
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.time.LocalDateTime
 
+@SpringBootTest
 @AutoConfigureMockMvc
 class AuthIntegrationTest : E2EDbInit() {
 
@@ -50,9 +51,9 @@ class AuthIntegrationTest : E2EDbInit() {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(credentialsRegisterRequest)
         }
-        val mapper = jacksonObjectMapper()
+
         val registerResponse =
-            mapper.readValue(resultRegister.andReturn().response.contentAsString, ResponseData::class.java)
+            objectMapper.readValue(resultRegister.andReturn().response.contentAsString, ResponseData::class.java)
         val credentialsRequest = CredentialsRequest(
             username = credentialsRegisterRequest.username,
             password = credentialsRegisterRequest.password
@@ -85,9 +86,9 @@ class AuthIntegrationTest : E2EDbInit() {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(credentialsRegisterRequest)
         }
-        val mapper = jacksonObjectMapper()
+
         val registerResponse =
-            mapper.readValue(resultRegister.andReturn().response.contentAsString, ResponseData::class.java)
+            objectMapper.readValue(resultRegister.andReturn().response.contentAsString, ResponseData::class.java)
 
         val credentialsRequest = CredentialsRequest(
             username = credentialsRegisterRequest.username,
