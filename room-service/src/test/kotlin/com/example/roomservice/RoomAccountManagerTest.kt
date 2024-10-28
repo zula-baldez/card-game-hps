1684c46ad8ceea8947d8e9d02d5a4ffbed9851c7
Ivan Sobolev <isobolev@solanteq.com>
2024-10-28 23:57:47 +0300

    add tests for rooms

--- room-service/src/test/kotlin/com/example/roomservice/RoomAccountManagerTest.kt
+++ room-service/src/test/kotlin/com/example/roomservice/RoomAccountManagerTest.kt
@@ -1,25 +1,24 @@
 package com.example.roomservice
 
 import com.example.common.client.ReactivePersonalAccountClient
-import com.example.common.dto.personalaccout.UpdateAccountRoomRequest
-import com.example.common.dto.roomservice.AccountAction
-import com.example.common.exceptions.AccountNotFoundException
 import com.example.common.exceptions.ForbiddenOperationException
 import com.example.common.exceptions.RoomNotFoundException
 import com.example.roomservice.repository.*
 import com.example.roomservice.service.RoomAccountManagerImpl
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
+import org.mockito.Mockito.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 
 class RoomAccountManagerTest {
+    private val roomRepository: RoomRepository = mock(RoomRepository::class.java)
+    private val accountInRoomRepository: AccountInRoomRepository = mock(AccountInRoomRepository::class.java)
+    private val bannedAccountInRoomRepository: BannedAccountInRoomRepository =
+        mock(BannedAccountInRoomRepository::class.java)
+    private val personalAccountClient: ReactivePersonalAccountClient = mock(ReactivePersonalAccountClient::class.java)
 
-    private val roomRepository: RoomRepository = mock()
-    private val accountInRoomRepository: AccountInRoomRepository = mock()
-    private val bannedAccountInRoomRepository: BannedAccountInRoomRepository = mock()
-    private val personalAccountClient: ReactivePersonalAccountClient = mock()
     private val roomAccountManager = RoomAccountManagerImpl(
         roomRepository,
         accountInRoomRepository,
@@ -28,83 +27,11 @@ class RoomAccountManagerTest {
     )
 
     @Test
-    fun `addAccount should add account to room when conditions are met`() {
-        val roomId = 1L
-        val accountId = 2L
-        val room = RoomEntity(roomId, "Test Room", 1L, 5, 0)
-
-        whenever(roomRepository.findById(roomId)).thenReturn(Mono.just(room))
-        whenever(bannedAccountInRoomRepository.findAllByRoomId(roomId)).thenReturn(Flux.empty())
-        whenever(accountInRoomRepository.findAllByRoomId(roomId)).thenReturn(Flux.empty())
-        whenever(accountInRoomRepository.save(any())).thenReturn(Mono.just(AccountInRoomEntity(accountId, roomId)))
-        whenever(
-            personalAccountClient.updateAccountRoom(
-                accountId,
-                UpdateAccountRoomRequest(roomId)
-            )
-        ).thenReturn(Mono.empty())
-
-        StepVerifier.create(roomAccountManager.addAccount(roomId, accountId))
-            .expectComplete()
-            .verify()
-
-        verify(accountInRoomRepository).save(AccountInRoomEntity(accountId, roomId, true))
-        verify(personalAccountClient).updateAccountRoom(accountId, UpdateAccountRoomRequest(roomId))
-    }
-
-    @Test
-    fun `addAccount should return error when account is not requester`() {
-        val roomId = 1L
-        val accountId = 2L
-
-        StepVerifier.create(roomAccountManager.addAccount(roomId, accountId))
-            .expectError(ForbiddenOperationException::class.java)
-            .verify()
-    }
-
-    @Test
-    fun `addAccount should return error when room not found`() {
-        val roomId = 1L
-        val accountId = 2L
-
-        whenever(roomRepository.findById(roomId)).thenReturn(Mono.empty())
-
-        StepVerifier.create(roomAccountManager.addAccount(roomId, accountId))
-            .expectError(RoomNotFoundException::class.java)
-            .verify()
-    }
-
-    @Test
-    fun `addAccount should return error when account is banned`() {
-        val roomId = 1L
-        val accountId = 2L
-        val room = RoomEntity(roomId, "Test Room", 1L, 5, 0)
-
-        whenever(roomRepository.findById(roomId)).thenReturn(Mono.just(room))
-        whenever(bannedAccountInRoomRepository.findAllByRoomId(roomId)).thenReturn(
-            Flux.just(
-                BannedAccountInRoomEntity(
-                    1,
-                    accountId,
-                    roomId
-                )
-            )
-        )
-
-        StepVerifier.create(roomAccountManager.addAccount(roomId, accountId))
-            .expectError(ForbiddenOperationException::class.java)
-            .verify()
-    }
-
-    @Test
-    fun `removeAccount should remove account from room when conditions are met`() {
-        val roomId = 1L
-        val accountId = 2L
-        val requesterId = 1L
-        val room = RoomEntity(roomId, "Test Room", requesterId, 5, 0)
-        whenever(roomRepository.findById(roomId)).thenReturn(Mono.just(room))
-        whenever(accountInRoomRepository.findAllByRoomId(roomId)).thenReturn(
-            Flux.just(
+    fun `getAccountRoom should return roomId for given accountId`() {
+        val accountId = 1L
+        val roomId = 100L
+        `when`(accountInRoomRepository.findById(accountId)).thenReturn(
+            Mono.just(
                 AccountInRoomEntity(
                     accountId,
                     roomId
@@ -112,37 +39,44 @@ class RoomAccountManagerTest {
             )
         )
 
-        StepVerifier.create(roomAccountManager.removeAccount(roomId, accountId, AccountAction.KICK))
-            .expectComplete()
-            .verify()
+        val result = roomAccountManager.getAccountRoom(accountId).block()
 
+        assertEquals(roomId, result)
+        verify(accountInRoomRepository).findById(accountId)
+    }
+
+    @Test
+    fun `getAccountsInRoom should return list of accountIds for given roomId`() {
+        val roomId = 100L
+        val accountIds = listOf(1L, 2L, 3L)
+        `when`(accountInRoomRepository.findAllByRoomId(roomId)).thenReturn(
+            Flux.just(
+                AccountInRoomEntity(1L, roomId),
+                AccountInRoomEntity(2L, roomId),
+                AccountInRoomEntity(3L, roomId)
+            )
+        )
+
+        val result = roomAccountManager.getAccountsInRoom(roomId).collectList().block()
+
+        assertEquals(accountIds, result)
         verify(accountInRoomRepository).findAllByRoomId(roomId)
     }
 
     @Test
-    fun `removeAccount should return error when room not found`() {
-        val roomId = 1L
-        val accountId = 2L
+    fun `getBannedAccountsInRoom should return list of banned accountIds for given roomId`() {
+        val roomId = 100L
+        val bannedAccountIds = listOf(1L, 2L)
+        `when`(bannedAccountInRoomRepository.findAllByRoomId(roomId)).thenReturn(
+            Flux.just(
+                BannedAccountInRoomEntity(1L, 1L, roomId),
+                BannedAccountInRoomEntity(2L, 2L, roomId)
+            )
+        )
+        val result = roomAccountManager.getBannedAccountsInRoom(roomId).collectList().block()
 
-        whenever(roomRepository.findById(roomId)).thenReturn(Mono.empty())
-
-        StepVerifier.create(roomAccountManager.removeAccount(roomId, accountId, AccountAction.KICK))
-            .expectError(RoomNotFoundException::class.java)
-            .verify()
+        assertEquals(bannedAccountIds, result)
+        verify(bannedAccountInRoomRepository).findAllByRoomId(roomId)
     }
 
-    @Test
-    fun `removeAccount should return error when account not found in room`() {
-        val roomId = 1L
-        val accountId = 2L
-        val requesterId = 1L
-        val room = RoomEntity(roomId, "Test Room", requesterId, 5, 0)
-
-        whenever(roomRepository.findById(roomId)).thenReturn(Mono.just(room))
-        whenever(accountInRoomRepository.findAllByRoomId(roomId)).thenReturn(Flux.empty())
-
-        StepVerifier.create(roomAccountManager.removeAccount(roomId, accountId, AccountAction.KICK))
-            .expectError(AccountNotFoundException::class.java)
-            .verify()
-    }
 }
