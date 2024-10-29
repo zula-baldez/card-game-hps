0d41b5cf082c0b8becde2b7762d1e7db75888fd2
moyakkauntda <moyakkauntda@gmail.com>
2024-10-29 15:45:31 +0300

    e2e fixes

--- game-handler/src/test/kotlin/com/example/configuration/TestConfiguration.kt
+++ game-handler/src/test/kotlin/com/example/configuration/TestConfiguration.kt
@@ -2,6 +2,9 @@ package com.example.configuration
 
 import com.example.common.client.PersonalAccountClient
 import com.example.common.client.RoomServiceClient
+import com.example.common.dto.personalaccout.AccountDto
+import com.example.common.dto.roomservice.RoomDto
+import org.mockito.Mockito.lenient
 import org.mockito.Mockito.mock
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,13 +16,28 @@ class TestConfiguration {
     @Primary
     fun personalAccountClient(): PersonalAccountClient {
         return mock(PersonalAccountClient::class.java)
+
     }
 
     @Bean
     @Primary
     fun roomServiceClient(): RoomServiceClient {
-        return mock(RoomServiceClient::class.java)
+        var mock = mock(RoomServiceClient::class.java)
+        lenient().`when`(mock.findById(1)).thenReturn(
+            RoomDto(
+                1,
+                "name",
+                6,
+                3,
+                listOf(
+                    AccountDto(6, "name", 0, 1),
+                    AccountDto(7, "name", 0, 1),
+                    AccountDto(8, "name", 0, 1)
+                ),
+                1
+            )
+        )
+        return mock
     }
-
 
 }
--- game-handler/src/test/kotlin/com/example/gameserviceintegration/DistributionStrategyTest.kt
+++ game-handler/src/test/kotlin/com/example/gameserviceintegration/DistributionStrategyTest.kt
@@ -4,6 +4,7 @@ import com.example.common.StompIntegrationTestBase
 import com.example.common.client.PersonalAccountClient
 import com.example.common.client.RoomServiceClient
 import com.example.common.dto.personalaccout.AccountDto
+import com.example.common.dto.roomservice.RoomDto
 import com.example.gamehandlerservice.model.dto.MoveCardRequest
 import com.example.gamehandlerservice.model.dto.MoveCardResponse
 import com.example.gamehandlerservice.model.game.Card
@@ -17,6 +18,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.lenient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.simp.stomp.StompSession
 
@@ -30,8 +32,8 @@ class DistributionStrategyTest : StompIntegrationTestBase() {
     // eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQyIiwiZXhwIjoxNzM4Nzk2MTE2LCJpYXQiOjE3MzAxNTYxMTZ9.fqVbKKuCZ_KQVw521chLghk-GjEFGfaWbLQ8kAF7dxfP_syKHvnVJ3JVX1ctocsl7irN8AnPCSCZ1mo9Bywg7qrIXfBQ25fbVVW7V8QLHjrPSV8CAOw30vQLg_lXYLXOPYz5DAXBZD_nmMXxDIoqKpLPywgFE3OeQwBexZcPCXd1zAx_OSesO9KtDB8o4dka6YJhzS3am7PpZhTLWHsbr0qpPZQ37HiBwO5vQCEctsY54WuSpjKPncfn3Hz2Vbr6V-obSUlPpqSldPKLTrhKOXdSwdD8ssrxqgHnoHcEVJ5WUZOlq2WdT2XwjMvmLe9bnNG6TQahTZWFHB0kWoTcZQ
     // id 7 t3 t3
     // eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI3Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQzIiwiZXhwIjoxNzM4Nzk2MTM0LCJpYXQiOjE3MzAxNTYxMzR9.O7IBMuk5JrvULgAqFyW-iFz9l_qcsAiKQrWwU1LErZVHIyNQi8QQNR-3AbC9Qw9GXkdQ7QelHzzE-yb9Jhk-sdblzjY_liXgqueKPx2wOGmPEwwK7a2a2PJkpIBG9smoHRE-JvNSBcEWKH4LLiGcAWEdsnHjg6Uk96GM-Xq4UU7kf872d5jhuLVsjU2VVvJ8NUf0YFKOGPIWdY47dC_49XRopx6HP4QqB9e4RsJs8IqT1aCure9ZlfsQjMjx8VhJchu7INagf-s4_hgHjfhEp6YkZVstzvCQv3IwdPs0ZDasg6E8jKllMWhukwv3gxPNZwwK7aK3_mA2vQdI3Xbf3w
-    private val gameId = 1L
-    private val roomId = 1L
+    private var gameId = 1L
+    private var roomId = 1L
 
     private val game: GameHandler
         get() = gameHandlerRegistry.getGame(gameId) ?: throw IllegalArgumentException("No game found")
@@ -47,13 +49,27 @@ class DistributionStrategyTest : StompIntegrationTestBase() {
 
     @BeforeEach
     fun initData() {
-        var session = getClientStompSession(1, 6, "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI2Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQxIiwiZXhwIjoxNzM4Nzk1OTc4LCJpYXQiOjE3MzAxNTU5Nzh9.FoR2FdXcBzVIl6zpk5_pb_igZXdKDk-watTbVG42Lqji1zBUpGyvFRC3sDjjlVYipLs2PvfL1Kt4gJz-UuxMECI7K42TNSi96kd_S4GEEs_7BK-quCsRgPmvHU-LgQsGPRrghZb7uYxKMfETidE9tRht67aoeHMnJCPdH-1YZ7UcjuO6EiX7asnYdtwTiUs98HXfLtIyMEG-dL3VekDW3Hwo3o5hIeuIvcvU2lRnFharWIkEmP4MqnN4gSVSt53Ft_6gJ1z2JrlK_OrsFupA9VG3uXi6ffHis7zX2VCis9EHUNAkrYIPtS6MUUmyBTo2shGgt7BxqNUVYb8hF-SuUA")
+        val gameHandler = gameHandlerRegistry.createGame("game", 1)
+
+        var session = getClientStompSession(
+            1,
+            6,
+            "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI2Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQxIiwiZXhwIjoxNzM4Nzk1OTc4LCJpYXQiOjE3MzAxNTU5Nzh9.FoR2FdXcBzVIl6zpk5_pb_igZXdKDk-watTbVG42Lqji1zBUpGyvFRC3sDjjlVYipLs2PvfL1Kt4gJz-UuxMECI7K42TNSi96kd_S4GEEs_7BK-quCsRgPmvHU-LgQsGPRrghZb7uYxKMfETidE9tRht67aoeHMnJCPdH-1YZ7UcjuO6EiX7asnYdtwTiUs98HXfLtIyMEG-dL3VekDW3Hwo3o5hIeuIvcvU2lRnFharWIkEmP4MqnN4gSVSt53Ft_6gJ1z2JrlK_OrsFupA9VG3uXi6ffHis7zX2VCis9EHUNAkrYIPtS6MUUmyBTo2shGgt7BxqNUVYb8hF-SuUA"
+        )
         userSessions[hostId] = session
 
-        session = getClientStompSession(roomId, 8, "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQyIiwiZXhwIjoxNzM4Nzk2MTE2LCJpYXQiOjE3MzAxNTYxMTZ9.fqVbKKuCZ_KQVw521chLghk-GjEFGfaWbLQ8kAF7dxfP_syKHvnVJ3JVX1ctocsl7irN8AnPCSCZ1mo9Bywg7qrIXfBQ25fbVVW7V8QLHjrPSV8CAOw30vQLg_lXYLXOPYz5DAXBZD_nmMXxDIoqKpLPywgFE3OeQwBexZcPCXd1zAx_OSesO9KtDB8o4dka6YJhzS3am7PpZhTLWHsbr0qpPZQ37HiBwO5vQCEctsY54WuSpjKPncfn3Hz2Vbr6V-obSUlPpqSldPKLTrhKOXdSwdD8ssrxqgHnoHcEVJ5WUZOlq2WdT2XwjMvmLe9bnNG6TQahTZWFHB0kWoTcZQ")
+        session = getClientStompSession(
+            roomId,
+            8,
+            "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQyIiwiZXhwIjoxNzM4Nzk2MTE2LCJpYXQiOjE3MzAxNTYxMTZ9.fqVbKKuCZ_KQVw521chLghk-GjEFGfaWbLQ8kAF7dxfP_syKHvnVJ3JVX1ctocsl7irN8AnPCSCZ1mo9Bywg7qrIXfBQ25fbVVW7V8QLHjrPSV8CAOw30vQLg_lXYLXOPYz5DAXBZD_nmMXxDIoqKpLPywgFE3OeQwBexZcPCXd1zAx_OSesO9KtDB8o4dka6YJhzS3am7PpZhTLWHsbr0qpPZQ37HiBwO5vQCEctsY54WuSpjKPncfn3Hz2Vbr6V-obSUlPpqSldPKLTrhKOXdSwdD8ssrxqgHnoHcEVJ5WUZOlq2WdT2XwjMvmLe9bnNG6TQahTZWFHB0kWoTcZQ"
+        )
         userSessions[8] = session
 
-        session = getClientStompSession(roomId, 7, "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI3Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQzIiwiZXhwIjoxNzM4Nzk2MTM0LCJpYXQiOjE3MzAxNTYxMzR9.O7IBMuk5JrvULgAqFyW-iFz9l_qcsAiKQrWwU1LErZVHIyNQi8QQNR-3AbC9Qw9GXkdQ7QelHzzE-yb9Jhk-sdblzjY_liXgqueKPx2wOGmPEwwK7a2a2PJkpIBG9smoHRE-JvNSBcEWKH4LLiGcAWEdsnHjg6Uk96GM-Xq4UU7kf872d5jhuLVsjU2VVvJ8NUf0YFKOGPIWdY47dC_49XRopx6HP4QqB9e4RsJs8IqT1aCure9ZlfsQjMjx8VhJchu7INagf-s4_hgHjfhEp6YkZVstzvCQv3IwdPs0ZDasg6E8jKllMWhukwv3gxPNZwwK7aK3_mA2vQdI3Xbf3w")
+        session = getClientStompSession(
+            roomId,
+            7,
+            "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI3Iiwic2VydmljZSI6InVzZXItdG9rZW4iLCJzY29wZSI6IlVTRVIiLCJpc3MiOiJzZWxmIiwibmFtZSI6InQzIiwiZXhwIjoxNzM4Nzk2MTM0LCJpYXQiOjE3MzAxNTYxMzR9.O7IBMuk5JrvULgAqFyW-iFz9l_qcsAiKQrWwU1LErZVHIyNQi8QQNR-3AbC9Qw9GXkdQ7QelHzzE-yb9Jhk-sdblzjY_liXgqueKPx2wOGmPEwwK7a2a2PJkpIBG9smoHRE-JvNSBcEWKH4LLiGcAWEdsnHjg6Uk96GM-Xq4UU7kf872d5jhuLVsjU2VVvJ8NUf0YFKOGPIWdY47dC_49XRopx6HP4QqB9e4RsJs8IqT1aCure9ZlfsQjMjx8VhJchu7INagf-s4_hgHjfhEp6YkZVstzvCQv3IwdPs0ZDasg6E8jKllMWhukwv3gxPNZwwK7aK3_mA2vQdI3Xbf3w"
+        )
         userSessions[7] = session
 
 
