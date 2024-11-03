df9c32d40710728c0284d15eff6dbf0700099add
Ivan Sobolev <isobolev@solanteq.com>
2024-11-03 20:42:46 +0300

    add tests for game-handler

--- game-handler/src/test/kotlin/com/example/gamehandlerservice/FinesCounterTest.kt
+++ game-handler/src/test/kotlin/com/example/gamehandlerservice/FinesCounterTest.kt
@@ -1,9 +1,9 @@
 package com.example.gamehandlerservice
 
+import com.example.common.client.PersonalAccountClient
 import com.example.common.dto.personalaccout.AccountDto
 import com.example.gamehandlerservice.model.game.Card
 import com.example.gamehandlerservice.model.game.CardDropResult
-import com.example.gamehandlerservice.model.game.Suit
 import com.example.gamehandlerservice.service.game.game.GameHandler
 import com.example.gamehandlerservice.service.game.model.GameData
 import com.example.gamehandlerservice.service.game.util.CyclicQueue
@@ -11,34 +11,26 @@ import com.example.gamehandlerservice.service.game.util.FinesCounter
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mock
-import org.mockito.Mockito.*
-
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 class FinesCounterTest {
 
     private lateinit var finesCounter: FinesCounter
-
-    @Mock
+    private lateinit var personalAccountClient: PersonalAccountClient
     private lateinit var gameHandler: GameHandler
-
-
-
-    private val fromId = 1L
-    private val toId = 2L
-    private val initialFines = 2
-    private lateinit var user: AccountDto
-    private lateinit var friend: AccountDto
+    private lateinit var accountFrom: AccountDto
+    private lateinit var accountTo: AccountDto
+    private lateinit var card: Card
     private lateinit var gameData: GameData
-    private val roomId: Long = 1L
-    private val gameId: Long = 100L
 
-
-    @BeforeEach
+            @BeforeEach
     fun setUp() {
+        personalAccountClient = mock(PersonalAccountClient::class.java)
+        finesCounter = FinesCounter(personalAccountClient)
         gameData = GameData(
-            gameId = gameId,
-            roomId = roomId,
+            gameId = 1L,
+            roomId = 1L,
             trump = null,
             playersTurnQueue = CyclicQueue(
                 listOf(
@@ -46,48 +38,34 @@ class FinesCounterTest {
                         name = "User1",
                         fines = 0,
                         id = 1L,
-                        roomId =1L
+                        roomId = 1L
                     )
                 )
             ),
             userCards = mutableMapOf(),
-            finesCounter = mutableMapOf()
+            finesCounter = mutableMapOf<Long, MutableMap<Long, Int>>()
         )
-        user = AccountDto(
-            name = "User1",
-            fines = initialFines,
-            id = fromId,
-            roomId = 1L
-        )
-        friend = AccountDto(
-            name = "User2",
-            fines = initialFines,
-            id = toId,
-            roomId = 1L
-        )
-
-        finesCounter = mock(FinesCounter::class.java)
         gameHandler = mock(GameHandler::class.java)
-
+        accountFrom = AccountDto(1, "fromAccountId", fines = 5, 1)
+        accountTo = AccountDto(2, "toAccountId", fines = 2, 1)
+        card = Card()
         `when`(gameHandler.gameData).thenReturn(gameData)
+
     }
 
     @Test
-    fun `giveFine should return valid when fines are less than target fines`() {
-        val card = Card(Suit.DIAMONDS, 6L, false)
-        val result = finesCounter.giveFine(gameHandler, user, friend, card)
-
-        assertEquals(null, result)
-        assertEquals(initialFines, friend.fines)
+    fun `should return valid when fine is added successfully`() {
+        val result = finesCounter.giveFine(gameHandler, accountFrom, accountTo, card)
+        assertEquals(CardDropResult.valid, result)
+        assertEquals(1, gameData.finesCounter[accountTo.id]?.get(accountFrom.id))
     }
 
     @Test
-    fun `giveFine should return invalid when current fines reach target fines`() {
-        val card = Card(Suit.DIAMONDS, 6L, false)
-        friend.fines = 0
-        val result = finesCounter.giveFine(gameHandler, user, friend, card)
+    fun `should return invalid when current fines are greater than or equal to accountTo fines`() {
+        gameData.finesCounter[accountTo.id] = mutableMapOf(accountFrom.id to 2)
 
-        assertEquals(null, result)
-        assertEquals(initialFines, user.fines)
+        val result = finesCounter.giveFine(gameHandler, accountFrom, accountTo, card)
+
+        assertEquals(CardDropResult.invalid, result)
     }
 }
