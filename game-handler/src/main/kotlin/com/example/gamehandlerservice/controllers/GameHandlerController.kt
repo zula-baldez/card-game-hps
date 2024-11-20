79473f12b2364ef84f91a99f8109db12d21a0281
Ivan Sobolev <isobolev@solanteq.com>
2024-11-20 21:49:07 +0300

    Add SecurityScheme to Game Handler

--- game-handler/src/main/kotlin/com/example/gamehandlerservice/controllers/GameHandlerController.kt
+++ game-handler/src/main/kotlin/com/example/gamehandlerservice/controllers/GameHandlerController.kt
@@ -4,11 +4,13 @@ import com.example.common.dto.CreateGameRequest
 import com.example.common.dto.CreateGameResponse
 import com.example.gamehandlerservice.service.game.registry.GameHandlerRegistry
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@SecurityRequirement(name = "Bearer Authentication")
 class GameHandlerController(
     private val registry: GameHandlerRegistry
 ) {
