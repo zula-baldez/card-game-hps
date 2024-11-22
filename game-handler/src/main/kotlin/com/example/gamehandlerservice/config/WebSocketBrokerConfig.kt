d0c4527013b77a9b8d2a7495ec88b471efa37497
moyakkauntda <moyakkauntda@gmail.com>
2024-11-22 20:55:36 +0300

    it works

---
+++ game-handler/src/main/kotlin/com/example/gamehandlerservice/config/WebSocketBrokerConfig.kt
@@ -0,0 +1,60 @@
+package com.example.gamehandlerservice.config
+
+import com.example.gamehandlerservice.service.game.registry.GameHandlerRegistry
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.messaging.converter.DefaultContentTypeResolver
+import org.springframework.messaging.converter.MappingJackson2MessageConverter
+import org.springframework.messaging.converter.MessageConverter
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver
+import org.springframework.messaging.simp.config.ChannelRegistration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.util.MimeTypeUtils
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableWebSocketMessageBroker
+class WebSocketBrokerConfig(
+    val userHandshakeInterceptor: UserHandshakeInterceptor,
+    val accountAndGameHandlerStompArgumentResolver: AccountAndGameHandlerStompArgumentResolver,
+    val objectMapper: ObjectMapper,
+    val interceptor: AuthorizationInterceptor,
+    val gameHandlerRegistry: GameHandlerRegistry
+) : WebSocketMessageBrokerConfigurer {
+    override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver?>) {
+        argumentResolvers.add(accountAndGameHandlerStompArgumentResolver)
+    }
+
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.enableSimpleBroker("/topic")
+        config.setApplicationDestinationPrefixes("/app")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/app/game")
+            .setAllowedOrigins("*")
+            .addInterceptors(userHandshakeInterceptor)
+    }
+
+    override fun configureMessageConverters(messageConverters: MutableList<MessageConverter?>): Boolean {
+        val resolver = DefaultContentTypeResolver()
+        resolver.defaultMimeType = MimeTypeUtils.APPLICATION_JSON
+        val converter = MappingJackson2MessageConverter()
+        converter.objectMapper = objectMapper
+        converter.contentTypeResolver = resolver
+        messageConverters.add(converter)
+        return false
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(interceptor)
+    }
+
+}
+
