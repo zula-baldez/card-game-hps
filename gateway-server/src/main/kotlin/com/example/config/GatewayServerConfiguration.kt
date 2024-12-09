16f04cb55a1406417a59fdc0d87065d54a5e60ba
moyakkauntda <moyakkauntda@gmail.com>
2024-12-09 21:14:35 +0300

    balancer configs

--- gateway-server/src/main/kotlin/com/example/config/GatewayServerConfiguration.kt
+++ gateway-server/src/main/kotlin/com/example/config/GatewayServerConfiguration.kt
@@ -11,28 +11,28 @@ class GatewayServerConfiguration {
     fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes()
             .route("personal-account") { r ->
-                r.path("/personal-account/**")
-                    .filters { f -> f.stripPrefix(1) }
+                r.path("/gateway/personal-account/**")
+                    .filters { f -> f.stripPrefix(2) }
                     .uri("lb://personal-account:8083")
             }
             .route("auth-service") { r ->
-                r.path("/auth-service/**")
-                    .filters { f -> f.stripPrefix(1) }
+                r.path("/gateway/auth-service/**")
+                    .filters { f -> f.stripPrefix(2) }
                     .uri("lb://auth-service:8081")
             }
             .route("game-handler") { r ->
-                r.path("/game-handler/**")
-                    .filters { f -> f.stripPrefix(1) }
+                r.path("/gateway/game-handler/**")
+                    .filters { f -> f.stripPrefix(2) }
                     .uri("lb://game-handler:8082")
             }
             .route("room-service") { r ->
-                r.path("/room-service/**")
-                    .filters { f -> f.stripPrefix(1) }
+                r.path("/gateway/room-service/**")
+                    .filters { f -> f.stripPrefix(2) }
                     .uri("lb://room-service:8084")
             }
             .route("avatars-service") { r ->
-                r.path("/avatars-service/**")
-                    .filters { f -> f.stripPrefix(1) }
+                r.path("/gateway/avatars-service/**")
+                    .filters { f -> f.stripPrefix(2) }
                     .uri("lb://avatars-service:8086")
             }
             .build()
